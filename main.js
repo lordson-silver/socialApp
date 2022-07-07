0ef544fe4cb87af1b3c6eff3ff06949bcefe0572
lordson-silver <lordsonochuko5@gmail.com>
2022-07-08 00:10:01 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+form.addEventListener("submit", (e) => {
+  console.log("Button Clicked");
+});
